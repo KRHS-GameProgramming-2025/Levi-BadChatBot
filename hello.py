6e47addf6b8093a290a92b70631357de8346f730
levi <levi.grady@kearsarge.org>
2024-09-05 08:54:43 -0400

    setup

---
+++ hello.py
@@ -0,0 +1,40 @@
+name = input ("what is your name? " )
+greeting = "hello"
+print(greeting + " " + name)
+color = input ("what is your favorite color? ")
+print("i really like " + color + " as well " + name)
+number = input ("what is your favorite number")
+
+if number == "21":
+    print("whats 9+10")
+else:
+    print("i also like " +  number +" "  +  name)
+
+tvshow = input ("what is your favorite tv show? ")
+if tvshow == "sponge bob":
+    print("do you now whats better then 24? 25")
+elif tvshow == "adventure time":
+    print("ITS ADVENTURE TIME!!!!")
+else:
+    print("i really like " + tvshow + " as well " + name)
+videogame = input ("what is your favorite videogame? ")
+if videogame == "super mario":
+    print("lets a go!")
+elif videogame == "earthbound":
+    print("welcome to moonside and goodbye")
+elif videogame == "tf2":
+    print("that medic is a spy!!!")
+elif videogame == "pikmin":
+    print("noot noot")
+elif videogame == "minecraft":
+    print("hmmmmmm")
+elif videogame == "plants vs zombies":
+    print("theres a zombie on your lawn")
+elif videogame == "parrappa the rapper":
+    print ("you gotta believe")
+elif videogame == "sonic":
+    print ("gotta go fast")
+elif videogame == "amoung us":
+    print("red was not the imposter")
+else:
+    print("i really like " + videogame + " as well " + name)
