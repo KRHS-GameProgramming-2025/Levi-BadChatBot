493539c95f8abb8fc0801a9c81344c747bb3e592
levi <levi.grady@kearsarge.org>
2024-09-10 08:37:41 -0400

    change a few problems

--- hello.py
+++ hello.py
@@ -1,40 +1,43 @@
-name = input ("what is your name? " )
-greeting = "hello"
+name = input ("What is your name? " )
+greeting = "Hello"
 print(greeting + " " + name)
-color = input ("what is your favorite color? ")
-print("i really like " + color + " as well " + name)
-number = input ("what is your favorite number")
+color = input ("What is your favorite color? ")
+print("I really like " + color + " as well " + name)
+number = input ("What is your favorite number")
 
 if number == "21":
-    print("whats 9+10")
+    print("Whats 9+10")
 else:
-    print("i also like " +  number +" "  +  name)
+    print("I also like " +  number +" "  +  name)
 
-tvshow = input ("what is your favorite tv show? ")
+tvshow = input ("What is your favorite tv show? ")
 if tvshow == "sponge bob":
-    print("do you now whats better then 24? 25")
+    print("Do you now whats better then 24? 25")
 elif tvshow == "adventure time":
     print("ITS ADVENTURE TIME!!!!")
 else:
-    print("i really like " + tvshow + " as well " + name)
-videogame = input ("what is your favorite videogame? ")
+    print("I really like " + tvshow + " as well " + name)
+videogame = input ("What is your favorite videogame? ")
 if videogame == "super mario":
-    print("lets a go!")
+    print("Lets a go!")
 elif videogame == "earthbound":
-    print("welcome to moonside and goodbye")
+    print("Welcome to Moonside and goodbye")
 elif videogame == "tf2":
-    print("that medic is a spy!!!")
+    print("That Medic is a Spy!!!")
 elif videogame == "pikmin":
-    print("noot noot")
+    print("Noot noot")
 elif videogame == "minecraft":
-    print("hmmmmmm")
+    print("Hmmmmmm")
 elif videogame == "plants vs zombies":
-    print("theres a zombie on your lawn")
+    print("Theres a Zombie on your lawn")
 elif videogame == "parrappa the rapper":
-    print ("you gotta believe")
+    print ("You gotta believe")
 elif videogame == "sonic":
-    print ("gotta go fast")
+    print ("Gotta go fast")
 elif videogame == "amoung us":
-    print("red was not the imposter")
+    print("Red was not the Imposter")
 else:
     print("i really like " + videogame + " as well " + name)
+blank = input (       )
+print(     )
+
